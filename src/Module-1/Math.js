b8ed31a79fa666ec523b5d589e2f7865b24b2473
nestorrg18 <neriga10@hotmail.com>
2023-04-18 17:52:11 -0600

    Clase 4: Reto_ altura de un triángulo

--- src/Module-1/Math.js
+++ src/Module-1/Math.js
@@ -1,6 +1,8 @@
 'use strict';
 
 // Formulas
+
+// Clase 2
 function square(side) {
     const perimeter = side * 4;
     const area = side ** 2;
@@ -21,6 +23,7 @@ function triangle(side1, side2, side3) {
 }
 triangle(5, 6, 8);
 
+// Clase 3
 function circle(radio) {
     const perimeter = 2 * radio * Math.PI;
     const area = (radio ** 2) * Math.PI;
@@ -29,3 +32,33 @@ function circle(radio) {
     console.log(result);
 }
 circle(7)
+
+// Clase 4 (altura de un triangulo isósceles no equilatero)
+function height(side1, base) {
+    if (side1 == base) {
+        console.warn('Este no es un triangulo isósceles no equilatero');
+    } else {
+        const height = Math.sqrt( (side1 ** 2) - ( (base ** 2) / 4) )
+    
+        const result = `Triangle side1: ${side1}, Triangle base: ${base} | height: ${height}`;
+        console.log(result);
+    }
+}
+height(6, 3)
+// reto (altura de un triangulo escaleno)
+function scalaneTriangleheight(side1, side2, side3) {
+    if (side1 == side2 || side1 == side3 || side2 == side3) {
+        console.warn('Este no es un triangulo escaleno');
+    } else {
+        // Heron formula
+        const perimeter = side1 + side2 + side3;
+        const S = perimeter / 2;
+        const heightSide1 = (2 / side1) * (Math.sqrt( S * (S - side1) * (S - side2) * (S - side3) ) );
+        const heightSide2 = (2 / side2) * (Math.sqrt( S * (S - side1) * (S - side2) * (S - side3) ) );
+        const heightSide3 = (2 / side3) * (Math.sqrt( S * (S - side1) * (S - side2) * (S - side3) ) );
+    
+        const result = `Triangle sides: ${side1}, ${side2}, ${side3},  | height Side1: ${heightSide1}, height Side2: ${heightSide2}, height Side3: ${heightSide3},`
+        console.log(result);
+    }
+}
+scalaneTriangleheight(3,4,5)
