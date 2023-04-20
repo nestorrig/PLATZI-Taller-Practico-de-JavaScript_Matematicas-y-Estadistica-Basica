1f1935e0e87deb186aec4e5571be5094b7379e5c
nestorrg18 <neriga10@hotmail.com>
2023-04-20 17:51:13 -0600

    Clase 7: Calculando descuentos con JavaScript

--- src/Module-1/percentage/script.js
+++ src/Module-1/percentage/script.js
@@ -2,6 +2,7 @@ const priceInput = document.getElementById("Price-input");
 const discountInput = document.getElementById("Discount-input");
 const button = document.getElementById("Percentage-button");
 const result = document.getElementById("Result");
+const message = document.getElementById("Message");
 
 const price = parseInt(priceInput.value)
 
@@ -12,11 +13,16 @@ button.addEventListener("click", CalculateDiscount)
 function CalculateDiscount() {
     const price = parseInt(priceInput.value)
     const discount = parseInt(discountInput.value)
-    
-    const newPrice = (price * (100 - discount)) / 100;
+    if (!price || !discount) {
+        message.innerText = 'Please intert all data';
+        return
+    }
+    if (discount > 100) {
+        message.innerText = "Discount can't be more than 100%";
+        return
+    }    
+    const newPrice = (price * (100 - discount)) / 100 || 0;
 
-    result.innerHTML = `$${newPrice}`
-    
-    // const result = `Original amount: ${price}, discount: %${discount} | New price: ${newPrice}`
-    // console.log(result);
+    result.innerText = `$${newPrice}`
+    message.innerText = ''
 }
