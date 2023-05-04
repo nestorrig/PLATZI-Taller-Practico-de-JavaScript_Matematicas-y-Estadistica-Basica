3691856b2e8ed4bd86ec42c76788bd5f44c1463c
nestorrg18 <neriga10@hotmail.com>
2023-05-03 20:43:43 -0600

    Clase 13: Método reduce; con reto

--- src/basicEstadistics/average.js
+++ src/basicEstadistics/average.js
@@ -3,13 +3,14 @@ const numbers = [12, 56, 89, 56, 78, 23, 56, 12, 23, 24, 71, 55, 20, 41, 46]
 const numbers2 = [1, 2, 3, 4, 5]
 
 function average(array) {
-    let sum = 0
-    for (let i = 0; i < array.length; i++) {
-        const element = array[i];
-        sum += element;
-    }
+    const sum = array.reduce(
+        (accumulator, currentValue) => accumulator + currentValue, 10
+    )
     const average = sum / array.length
     return average
 }
 average(numbers) // 44.133333
 average(numbers2) // 3
+// saber si un array es par o no
+const isEven = array => array.length % 2 == 0 ? true : false
+isEven(numbers)
