fca47c9bd93abc0b29734bea7b764dca2ac50ccc
nestorrg18 <neriga10@hotmail.com>
2023-05-08 21:12:26 -0600

    Clase 14: Calculando la mediana en una lista impar; reto lista par

--- src/basicEstadistics/average.js
+++ src/basicEstadistics/average.js
@@ -1,6 +1,6 @@
 'use strict';
-const numbers = [12, 56, 89, 56, 78, 23, 56, 12, 23, 24, 71, 55, 20, 41, 46]
-const numbers2 = [1, 2, 3, 4, 5]
+const numbers = [12, 56, 89, 56, 78, 23, 56, 12, 23, 24, 71, 55, 20, 41, 46, 100]
+const numbers2 = [10, 13, 2, 50, 80, 3, 15, 43, 5, 6, 7]
 
 function average(array) {
     const sum = array.reduce(
@@ -17,13 +17,17 @@ const isEven = array => array.length % 2 == 0 ? true : false
 isEven(numbers)
 
 function mediana(array) {
+    let indexMediana;
+    let median
     const newArray  = array.sort((a, b) => a - b);
     const isEven = array.length % 2 == 0 ? true : false
     if (isEven) {
-
+        indexMediana = newArray.length / 2;
+        median = average( [newArray[indexMediana], newArray[indexMediana - 1] ] )
+        console.log(median);
     } else {
-        const indexMediana = Math.floor(newArray.length / 2);
-        const median = newArray[indexMediana]
+        indexMediana = Math.floor(newArray.length / 2);
+        median = newArray[indexMediana]
         console.log(median);
     }
     console.log(newArray);
