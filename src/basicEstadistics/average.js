c174ef85e7057e031de802f82f7be32e8e1aace7
nestorrg18 <neriga10@hotmail.com>
2023-05-10 16:49:25 -0600

    Clase 15: Calculando la mediana en una lista par

--- src/basicEstadistics/average.js
+++ src/basicEstadistics/average.js
@@ -1,6 +1,6 @@
 'use strict';
-const numbers = [12, 56, 89, 56, 78, 23, 56, 12, 23, 24, 71, 55, 20, 41, 46, 100]
-const numbers2 = [10, 13, 2, 50, 80, 3, 15, 43, 5, 6, 7]
+const numbers = [12, 56, 56, 78, 23, 56, 12, 23, 24, 71, 55, 20, 41, 46]
+const numbers2 = [9, 10, 12, 3, 1, 6, 8]
 
 function average(array) {
     const sum = array.reduce(
@@ -9,7 +9,7 @@ function average(array) {
     const average = sum / array.length
     return average
 }
-average(numbers) // 44.133333
+average(numbers) // 44.13
 average(numbers2) // 3
 
 // saber si un array es par o no
@@ -17,20 +17,20 @@ const isEven = array => array.length % 2 == 0 ? true : false
 isEven(numbers)
 
 function mediana(array) {
-    let indexMediana;
     let median
-    const newArray  = array.sort((a, b) => a - b);
-    const isEven = array.length % 2 == 0 ? true : false
-    if (isEven) {
-        indexMediana = newArray.length / 2;
-        median = average( [newArray[indexMediana], newArray[indexMediana - 1] ] )
-        console.log(median);
+    const arrayB  = array.sort((a, b) => a - b);
+    
+    if (isEven(array)) {
+        const index1 = arrayB.length / 2;
+        const index2 = (arrayB.length / 2) - 1;
+        const list = [arrayB[index1], arrayB[index2]]
+        median = average(list)
     } else {
-        indexMediana = Math.floor(newArray.length / 2);
-        median = newArray[indexMediana]
-        console.log(median);
+        let indexMediana = Math.floor(arrayB.length / 2);
+        median = arrayB[indexMediana]
     }
-    console.log(newArray);
+    console.log(arrayB);
+    console.log(median);
 }
 mediana(numbers)
 mediana(numbers2)
