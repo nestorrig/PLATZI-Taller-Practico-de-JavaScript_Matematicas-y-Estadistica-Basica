56983af3c193a46976d06296cde847e7cd77067b
nestorrg18 <neriga10@hotmail.com>
2023-05-19 19:20:49 -0600

    Clase 16: Calculando la moda: objetos a partir de arrays

--- src/basicEstadistics/average.js
+++ src/basicEstadistics/average.js
@@ -1,6 +1,7 @@
 'use strict';
 const numbers = [12, 56, 56, 78, 23, 56, 12, 23, 24, 71, 55, 20, 41, 46]
 const numbers2 = [9, 10, 12, 3, 1, 6, 8]
+const numbers3 = [1,1,2,3,5,5,5,5,6,9,9,8,8,1,4,4,5,5,5,1,1,1,1]
 
 function average(array) {
     const sum = array.reduce(
@@ -34,3 +35,31 @@ function mediana(array) {
 }
 mediana(numbers)
 mediana(numbers2)
+
+function mode(array) {
+
+    const frecuency = {};
+
+    array.forEach(number => {
+        // frecuency[number] = (frecuency[number] || 0) + 1;
+        if (frecuency[number]) {
+            frecuency[number] += 1;
+        } else {
+            frecuency[number] = 1;
+        }
+    });
+
+    console.log(frecuency);
+    let mode = [];
+    let maxFrecuency = 0;
+    for (const num in frecuency) {
+        if (frecuency[num] > maxFrecuency) {
+            mode = [parseInt(num)];
+            maxFrecuency = frecuency[num]
+        } else if(frecuency[num] === maxFrecuency) {
+            mode.push(parseInt(num));
+        }
+    }
+    console.log(mode);
+}
+mode(numbers3)
