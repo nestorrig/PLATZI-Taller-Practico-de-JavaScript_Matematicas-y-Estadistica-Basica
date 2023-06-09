26f33c09e99f1cdf07ad8bd1d664f2a05111e45d
nestorrig <neriga10@hotmail.com>
2023-06-09 17:10:16 -0600

    Clase 23: Análisis salarial con JavaScript; reto completado

--- src/salaries/analysis.js
+++ src/salaries/analysis.js
@@ -6,13 +6,39 @@ function findPerson(personId) {
 function findPersonByName(personName) {
     return salaries.find(person => person.name === personName);
 }
-
-function mediaPerPersone(personId) {
+function salaryHistory(personId) {
     const jobs = findPerson(personId).jobs;
-    const salaryHistory = jobs.map(element => {
+    const salary= jobs.map(element => {
         return element.salary
     })
-    const medianSalary = PlatziMath.mediana(salaryHistory)
-    console.log(salaryHistory);
+    return salary
+}
+function mediaPerPersone(personId) {
+    // const jobs = findPerson(personId).jobs;
+    const salary = salaryHistory(personId)
+    const medianSalary = PlatziMath.mediana(salary)
     return medianSalary
+}
+function salaryProjetion(personId) {
+    const jobs = findPerson(personId).jobs;
+    const salary = jobs.map(element => {
+        return element.salary
+    })
+    return increasesAndDecreases(salary)
+}
+function increasesAndDecreases(array) {
+    const arrayb = [];
+    for (let i = 0; i < array.length; i++) {
+        const element = array[i];
+        if (i > 0 ) {
+            let differenceAbsolute = element - array[i-1]
+            let differenceRelative = Math.ceil(( differenceAbsolute / array[i-1]) * 100)// esto me da el porcentaje
+            arrayb.push(differenceRelative)
+            console.log(differenceRelative)
+        } 
+    }
+    let nextIncreaseOrDecrease = PlatziMath.average(arrayb)
+    let newSalary = Math.ceil(array.at(-1) + (array.at(-1) * (nextIncreaseOrDecrease / 100)))
+    console.log(array.at(-1), nextIncreaseOrDecrease);
+    return newSalary
 }
