01a04f93a8f16e0ccda2fe60e3c6e141f363b281
nestorrig <neriga10@hotmail.com>
2023-07-06 20:46:27 -0600

    Proyecto final v0.1, Mostrando y ocultando secciones secundarias (Geometry y Discounts)

--- js/index.js
+++ js/index.js
@@ -1,21 +1,32 @@
 const sections = document.getElementsByClassName("section-item")
-// console.log(sections);
-const links = document.getElementsByTagName("a")
-// console.log(links);
-for (const iterator of links) {
-    const reference = iterator.attributes.href.value
-    const referenceWithoutId = Array.from(reference)
-    referenceWithoutId.shift()
-    const realReference = referenceWithoutId.join('')
-    // console.log(realReference);
-    iterator.addEventListener('click', () => {hideAndShowSection(realReference)})
-}
+const links = document.getElementsByClassName("section-link")
+const geometrySections = document.getElementsByClassName("geometry-item");
+const geometryLinks = document.getElementsByClassName("geometry-link")
+const triangleSections = document.getElementsByClassName("triangle-item");
+const triangleLinks = document.getElementsByClassName("triangle-link");
+const discountSections = document.getElementsByClassName("discount-container");
+const discountLinks = document.getElementsByClassName("discount-link");
 
-function hideAndShowSection(reference) {
-    console.log(reference);
+function matchLinks(array, sections) {
+    for (const iterator of array) {
+        const reference = iterator.attributes.href.value
+        const referenceWithoutId = Array.from(reference)
+        referenceWithoutId.shift()
+        const realReference = referenceWithoutId.join('')
+        // console.log(realReference);
+        iterator.addEventListener('click', () => {hideAndShowSection(realReference, sections)})
+    }
+}
+matchLinks(links, sections)
+matchLinks(geometryLinks, geometrySections)
+matchLinks(triangleLinks, triangleSections)
+matchLinks(discountLinks, discountSections)
+
+function hideAndShowSection(references, sections) {
+    console.log(references);
     for (const iterator of sections) {
         const identification = iterator.id
-        if (identification != reference) {
+        if (identification != references) {
             iterator.classList.add("inactive")
         } else {
             iterator.classList.remove("inactive")
