0c53cd9301987c8e673465d21b6786f7414d47d8
nestorrig <neriga10@hotmail.com>
2023-07-10 22:25:16 -0600

    Proyecto final v0.1, fixes: semicolons added and console.log removed

--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
-const sections = document.getElementsByClassName("section-item")
-const links = document.getElementsByClassName("section-link")
+'use strict';
+
+const sections = document.getElementsByClassName("section-item");
+const links = document.getElementsByClassName("section-link");
 const geometrySections = document.getElementsByClassName("geometry-item");
 const geometryLinks = document.getElementsByClassName("geometry-link")
 const triangleSections = document.getElementsByClassName("triangle-item");
@@ -9,27 +11,25 @@ const discountLinks = document.getElementsByClassName("discount-link");
 
 function matchLinks(links, sections) {
     for (const iterator of links) {
-        const reference = iterator.attributes.href.value
-        const referenceWithoutId = Array.from(reference)
-        referenceWithoutId.shift()
-        const realReference = referenceWithoutId.join('')
-        // console.log(realReference);
-        iterator.addEventListener('click', () => {hideAndShowSection(realReference, sections)})
+        const reference = iterator.attributes.href.value;
+        const referenceWithoutId = Array.from(reference);
+        referenceWithoutId.shift();
+        const realReference = referenceWithoutId.join('');
+        iterator.addEventListener('click', () => {hideAndShowSection(realReference, sections)});
     }
 }
-matchLinks(links, sections)
-matchLinks(geometryLinks, geometrySections)
-matchLinks(triangleLinks, triangleSections)
-matchLinks(discountLinks, discountSections)
+matchLinks(links, sections);
+matchLinks(geometryLinks, geometrySections);
+matchLinks(triangleLinks, triangleSections);
+matchLinks(discountLinks, discountSections);
 
 function hideAndShowSection(references, sections) {
-    console.log(references);
     for (const iterator of sections) {
-        const identification = iterator.id
+        const identification = iterator.id;
         if (identification != references) {
-            iterator.classList.add("inactive")
+            iterator.classList.add("inactive");
         } else {
-            iterator.classList.remove("inactive")
+            iterator.classList.remove("inactive");
         }
     }
 }
